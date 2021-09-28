7faf18f8f3fae81a7a21d613cf1632df96268dd9
김민지 : ) <happyhill99@gmail.com>
2021-09-28 15:39:03 +0900

    0928

--- 7-2.py
+++ 7-2.py
@@ -1,2 +1,14 @@
 import random
-com
+com=random.randint(1,20)
+print('<<컴퓨터가 생각한 1~20 숫자 맞추기>>')
+while True:
+    player = int(input('숫자 입력(종료 0): '))
+    if player == 0 :
+        break
+    elif player == com :
+        print('정답!!')
+        break
+    elif player > com :
+        print('더 작은 숫자 입력!')
+    elif player < com :
+        print('더 큰 숫자 입력!')
---
+++ 7-4.py
@@ -0,0 +1,2 @@
+for i in range(5):
+    print(str(5-i)*(5-i))
