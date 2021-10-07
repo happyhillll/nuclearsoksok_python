f86c8f7352b3c5f2f6c10ad04ec8a3a09dde0e0b
김민지 : ) <happyhill99@gmail.com>
2021-10-07 16:16:56 +0900

    1007

--- 10장 도전 1-2.py
+++ 10장 도전 1-2.py
@@ -1,7 +1,8 @@
 f=open('data3.txt','a')
 for i in range(3):
     w=input('word:')
-    
+    if not w:break
+    f.write(w+'')
 f.close()
 f=open('data3.txt')
 cnt=0
@@ -9,7 +10,7 @@ for line in f:
     line=line.rstrip()
     word=line.split()
     for w in word:
-        
+        cnt += 1
         print(w,end='')
 print()
 print('total word :',cnt)
