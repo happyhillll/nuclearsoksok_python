0b612c16f56074500caab57d6dca37f8b4b4828e
김민지 : ) <happyhill99@gmail.com>
2021-10-11 00:06:22 +0900

    Update 10장 도전 2-1.py

--- 10장 도전 2-1.py
+++ 10장 도전 2-1.py
@@ -1,22 +1,20 @@
 f=open('score2.txt')
+print('이름,평균,학점')
 for line in f:
     name,score=line.split()
+    for i in line:
+        if int(score)>=90:
+            grade="A"
+        elif int(score)>=80:
+            grade="B"
+        elif int(score)>=70:
+            grade="C"
+        elif int(score)>=60:
+            grade="D"
+        elif int(score)<60:
+            grade="F"
+    print(name,score,grade)
 
-for i in line:
-    if score>=90:
-        grade=A
-    elif score>=80:
-        grade=B
-    elif score>=70:
-        grade=C
-    elif score>=60:
-        grade=D
-    elif score<60:
-        grade=F
+#인덴트 조심하기!!!!!!
 
-print('이름,평균,학점')
-
-while True:
-    for k in f:
-        print(name,score,grade)
 
